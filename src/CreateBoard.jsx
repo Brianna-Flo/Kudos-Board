be0a9fefe88590d2249025105491ae6165e2e4c4
Brianna Flores <briannaflores@meta.com>
2025-06-15 20:29:28 -0700

    this commit will add additional inputs for the create board form

--- src/CreateBoard.jsx
+++ src/CreateBoard.jsx
@@ -12,14 +12,18 @@ const CreateBoard = ({onCloseModal}) => {
                 <form className='create-board-form'>
                     <label for='title'>Title:</label>
                     <input type='text' name='title' id='title' required/>
+                    <label for='description'>Description:</label>
+                    <input type='text' name='description' id='description' required/>
                     <label>Category:</label>
-                    <select id="category" name='category'>
+                    <select id="category" name='category' required>
                         {/* TODO change to map to list of categories?? */}
                         <option value=''>Select a category</option>
                         <option id='celebration' value='celebration'>Celebration</option>
                         <option id='thank-you' value='thank-you'>Thank You</option>
                         <option id="inspiration" value="inspiration">Inspiration</option>
                     </select>
+                    <label for='image'>Image:</label>
+                    <input type='file' name='image' id='image' required/>
                     <label>Author:</label>
                     <input type='text' name='author' id='author'/>
                     <button className='create-board-btn' type='submit'>Create Board</button>
