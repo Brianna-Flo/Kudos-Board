74f843df32ed4d8a95bac2591668972c6f87a491
Brianna Flores <briannaflores@meta.com>
2025-06-15 20:20:44 -0700

    this commit will style the create board modal

--- src/CreateBoard.jsx
+++ src/CreateBoard.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+import './CreateBoard.css'
 
 const CreateBoard = ({onCloseModal}) => {
 
 
     return (
-        <div className='create-board-modal'>
-            <button className='create-board-close' onClick={onCloseModal}>&times;</button>
-            <h2>Create a New Board</h2>
-            <form className='create-board-form'>
-                <label for='title'>Title:</label>
-                <input type='text' name='title' id='title' required/>
-                <label>Category:</label>
-                <select id="category" name='category'>
-                    {/* TODO change to map to list of categories?? */}
-                    <option value=''>Select a category</option>
-                    <option id='celebration' value='celebration'>Celebration</option>
-                    <option id='thank-you' value='thank-you'>Thank You</option>
-                    <option id="inspiration" value="inspiration">Inspiration</option>
-                </select>
-                <label>Author:</label>
-                <input type='text' name='author' id='author'/>
-                <button className='create-board-btn' type='submit'>Create Board</button>
-            </form>
-        </div>
+        <section className='modal' id='create-board-modal'>
+            <div className='modal-content'>
+                <button className='modal-close' onClick={onCloseModal}>&times;</button>
+                <h2>Create a New Board</h2>
+                <form className='create-board-form'>
+                    <label for='title'>Title:</label>
+                    <input type='text' name='title' id='title' required/>
+                    <label>Category:</label>
+                    <select id="category" name='category'>
+                        {/* TODO change to map to list of categories?? */}
+                        <option value=''>Select a category</option>
+                        <option id='celebration' value='celebration'>Celebration</option>
+                        <option id='thank-you' value='thank-you'>Thank You</option>
+                        <option id="inspiration" value="inspiration">Inspiration</option>
+                    </select>
+                    <label>Author:</label>
+                    <input type='text' name='author' id='author'/>
+                    <button className='create-board-btn' type='submit'>Create Board</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
