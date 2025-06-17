6301c490acd06c255c5ee9100c188de0d3a24b46
Brianna Flores <briannaflores@meta.com>
2025-06-17 16:41:47 -0700

    this commit will modify the get methods to work with the database

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -3,38 +3,40 @@ const express = require("express");
 // create a router
 const router = express.Router();
 
-let kudosboards = [
-        {id: 1, title: "hello", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
-        {id: 2, title: "exciting", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
-        {id: 3, title: "wooo", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
-        {id: 4, title: ":P", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []}
-    ]
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+// let kudosboards = [
+//         {id: 1, title: "hello", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
+//         {id: 2, title: "exciting", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
+//         {id: 3, title: "wooo", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []},
+//         {id: 4, title: ":P", description: "descriptionsss", category: "Thank You", image: "jazz.jpg", author: "", cards: []}
+//     ]
 
 // get all boards from the database using routes
 // since were interacting with a database, use async/await
-router.get('/', (req, res) => {
-    // res.status(200).send(pets);
+router.get('/', async (req, res) => {
+    const kudosboards = await prisma.kudosboards.findMany()
     res.json(kudosboards)
 })
 
-router.get('/:boardId', (req, res) => {
-    // given a boards id, return that board pet
-    // grab petId from params in url line
+// get board by id
+router.get('/:boardId', async (req, res) => {
     const boardId = parseInt(req.params.boardId)
-    const board = kudosboards.find(board => 
-        board.id === boardId
-    )
-
-    // if pet exists, return into array
-    if (board) {
-        res.json(board);
-    } else {
-        res.status(404).send("Board not found")
-    }
+    const board = await prisma.kudosboards.findUnique({
+        where: {id: boardId}
+    })
+    res.json(board);
+    // // if pet exists, return into array
+    // if (board) {
+    //     res.json(board);
+    // } else {
+    //     res.status(404).send("Board not found")
+    // }
 })
 
 // post request, listens for create requests
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const {title, description, category, image, author, cards} = req.body;
 
     // create a new pet object
